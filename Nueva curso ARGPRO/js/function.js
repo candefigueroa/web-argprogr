14dcd2d70022a029327311b576ab7ccd455529b2
candefigueroa <126287340+candefigueroa@users.noreply.github.com>
2023-02-24 21:18:41 -0300

    Add files via upload

---
+++ Nueva curso ARGPRO/js/function.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML ="Telefono: (154) 9988775 Email:Juanajimenez20@example.com";
+} 
